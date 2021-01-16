47f1cfac45aa6db0300d98e1b21fe507794858d3
Dave Raymond <ne0busin3ss@gmail.com>
2021-01-16 16:26:49 -0600

    Add files via upload

--- grades.go
+++ grades.go
@@ -6,17 +6,33 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Print ("Enter a grade: ")
+	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
-	input, err:= reader.ReadString('\n')
+	input, err := reader.ReadString('\n')
 
 	if err != nil {
-		log.Fatal (err)
+		log.Fatal(err)
 	}
 
-	fmt.Println (input)
-}
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	{
+		fmt.Println("A grade of", grade, "is", status)
+
+	}
+}
