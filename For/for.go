5d63d182791fcbff3360064c365f9b3c2ae6a78f
ne0busin3ss <dave@dave-raymond.com>
2021-02-21 14:39:59 -0600

    First for loop to 3.

--- For/for.go
+++ For/for.go
@@ -9,7 +9,7 @@ func main() {
 
 	// The most basic type, with a single condition.
 	i := 1
-	for i <= 7 {
+	for i <= 3 {
 		fmt.Println(i)
 		i = i + 1
 	}
@@ -37,5 +37,6 @@ func main() {
 	}
 }
 
-/*'n' is the variable for the number. %2 == 0 tells go to divide the number by 2 and if the remainder is 0,
-it is even; continue to the next iteration -don't display even numbers.*/
+/*'n' is the variable for the number. %2 == 0 tells go to divide the number by 2 and
+if the remainder is 0, it is even; continue to the next iteration -don't display even
+numbers.*/
