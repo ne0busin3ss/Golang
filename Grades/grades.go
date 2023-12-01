32b1c7f061a8088f175c2f27ebf664915e6507d6
Dave Raymond <ne0busin3ss@gmail.com>
2023-11-30 20:05:55 -0600

    Add files via upload

---
+++ Grades/grades.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("Please enter a grade:")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if grade >= 70 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	{
+		fmt.Println("A grade of", grade, "is", status)
+
+	}
+}
