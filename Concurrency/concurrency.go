9196ac7ae3893a60f2b4f07dc0075216a49ed512
ne0busin3ss <dave@dave-raymond.com>
2025-07-12 08:03:54 -0500

    Windsurf updates

--- Concurrency/concurrency.go
+++ Concurrency/concurrency.go
@@ -1,33 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func readword(ch chan string) {
+func readword(ch chan<- string) {
 	fmt.Println("Type a word, then hit Enter.")
 	var word string
+	// In a real-world application, you should handle the error returned by Scanf.
 	fmt.Scanf("%s", &word)
 	ch <- word
 }
 
-func timeout(t chan bool) {
-	time.Sleep(10 * time.Second)
-	t <- false
-}
-
 func main() {
-	t := make(chan bool)
-	go timeout(t)
+	// Create a context that will be cancelled after 5 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel() // It's important to call cancel to release resources.
 
-	ch := make(chan string)
+	// Use a buffered channel to prevent the readword goroutine from leaking.
+	ch := make(chan string, 1)
 	go readword(ch)
 
 	select {
 	case word := <-ch:
 		fmt.Println("Received", word)
-	case <-t:
+	case <-ctx.Done():
 		fmt.Println("Timeout.")
 	}
 }
